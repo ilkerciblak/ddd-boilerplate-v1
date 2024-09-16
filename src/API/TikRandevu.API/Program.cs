0eada60c9a0e4d6c30582b9e7a529d1e668d91c8
ilkerciblak <ilkerciblak@gmail.com>
2024-09-16 17:08:38 +0300

    fix(logging): add injection for logging

--- src/API/TikRandevu.API/Program.cs
+++ src/API/TikRandevu.API/Program.cs
@@ -1,10 +1,19 @@
+using Serilog;
 using TikRandevu.API.Middlewares;
 using TikRandevu.Shared.Application;
 using TikRandevu.Shared.Infrastructure;
 using TikRandevu.Shared.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Host.UseSerilog(((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+}));
+
 var services = builder.Services;
+
 
 
 services.ApplySharedApplicationServices([]);
--- src/Shared/TikRandevu.Shared.Domain/obj/Debug/net8.0/TikRandevu.Shared.Domain.AssemblyInfo.cs
+++ src/Shared/TikRandevu.Shared.Domain/obj/Debug/net8.0/TikRandevu.Shared.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TikRandevu.Shared.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9926ff89030656d16c0954296d2893bce5f7e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf2dac7834e837253a06ec48b598054896f49ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("TikRandevu.Shared.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TikRandevu.Shared.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
